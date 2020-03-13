f6c3d822e98b73ed7ccefe1141c880d0bec8294e
stud <lykyanovdaniil198@gmail.com>
2020-03-13 10:24:32 +0700

    Payment

---
+++ WindowsProject1/WindowsProject1/businesslogic/Payment.cpp
@@ -0,0 +1,11 @@
+#include "Payment.h"
+
+Payment::Payment(Money cashTendered)
+{
+	amount = cashTendered;
+}
+
+Money Payment::getAmount()
+{
+	return amount;
+}
--- WindowsProject1/WindowsProject1/businesslogic/Payment.h
+++ WindowsProject1/WindowsProject1/businesslogic/Payment.h
@@ -1,1 +1,11 @@
 #pragma once
+#include "Money.h"
+class Payment
+{
+private:
+    Money amount;
+public:
+    Payment(Money cashTendered);
+    Money getAmount();
+};
+
